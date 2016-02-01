0e65ca2a366ddb4e57b3fc1994ec9ae6c4522077
wojciech-szymanski <woszymanski@gmail.com>
2016-02-01 23:01:32 +0000

    Added projects section and reviewed look and feel

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,14 @@
 /**
- * Created by fabiomadeira on 25/02/15.
+ * Created by Wojciech Szymanski
  */
-// jQuery for generating email link
-jQuery(document).ready(function(e) {
-    e("a.generate-mail").each(function() {
-    	var email = e(this).data('left') + '@' + e(this).data('right');
 
-		e(this).attr('href', 'mailto:' + email);
-    	if (e(this).hasClass('append-mail')) {
-    		e(this).append(email);
-    	}
-    });
-});
+window.onload = function() {
+	var links = document.getElementsByClassName("generate-mail");
 
+	Array.prototype.forEach.call(links, function(element) {
+		var email = element.getAttribute("data-left") + 
+			'@' + element.getAttribute("data-right");
 
+		element.setAttribute("href", "mailto:" + email);
+	});
+};
