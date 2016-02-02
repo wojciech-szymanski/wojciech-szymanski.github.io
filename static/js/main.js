d633a1de76ae113f2604527145a20860cc9276df
wojciech-szymanski <woszymanski@gmail.com>
2016-02-02 15:29:51 +0000

    Added parsing of external links and email link. Modified template to accomodate these changes.

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,31 @@
 /**
- * Created by Wojciech Szymanski
+ * @name parseLink
+ * @description Default function for modifying link element
+ * 
+ * @css_class {string} name of the CSS class that needs to be selected
+ * @callback {function} callback function to be executed on each matched element
+ *
+ * @function
+ * @returns {void} 
  */
+function parseLink(css_class, callback) {
+	var links = document.getElementsByClassName(css_class);
 
+	Array.prototype.forEach.call(links, callback);
+}
+
+/**
+ * Main load function
+ */
 window.onload = function() {
-	var links = document.getElementsByClassName("generate-mail");
 
-	Array.prototype.forEach.call(links, function(element) {
+	// Set external links to open in new window
+	parseLink("external-link", function(element) {
+		element.setAttribute("target", "_blank");
+	});
+
+	// Generate mailto link from data fragments
+	parseLink("generate-email", function(element) {
 		var email = element.getAttribute("data-left") + 
 			'@' + element.getAttribute("data-right");
 
