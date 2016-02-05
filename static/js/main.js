9c29189fae2071d8b2dd1698872f46cdfdfb04ed
wojciech-szymanski <woszymanski@gmail.com>
2016-02-05 18:43:53 +0000

    Modified projects to open in an overlay
    Updated markup on other pages to accomodate the changes

--- static/js/main.js
+++ static/js/main.js
@@ -15,6 +15,53 @@ function parseLink(css_class, callback) {
 }
 
 /**
+ * @name openOverlay
+ * @description Assigns event handler for opening project details in an overlay
+ *
+ * @function
+ * @returns {void} 
+ */
+function openOverlay() {
+	var items = document.getElementsByClassName("item");
+
+	Array.prototype.forEach.call(items, processLinks);
+
+	function processLinks(item) {
+		var content = item.getElementsByClassName("item-content")[0].innerHTML;
+		var links = item.getElementsByClassName("open-overlay");
+
+		Array.prototype.forEach.call(links, addEvent.bind(undefined, content));
+	}
+
+	function addEvent(content, link) {
+		link.addEventListener("click", onClick.bind(undefined, content));
+	}
+
+	function onClick(content, e) {
+		e.preventDefault();
+		document.getElementsByClassName("project-details")[0].innerHTML = content;
+		document.getElementById("overlay").classList.remove("hide");
+	}
+
+}
+
+/**
+ * @name closeOverlay
+ * @description Assigns event handler for closing an overlay
+ *
+ * @function
+ * @returns {void} 
+ */
+function closeOverlay() {
+	var close = document.getElementsByClassName("overlay-close")[0].getElementsByTagName("a")[0];
+
+	close.addEventListener("click", function(e) {
+		e.preventDefault();
+		document.getElementById("overlay").classList.add("hide");
+	})
+}
+
+/**
  * Main load function
  */
 window.onload = function() {
@@ -31,4 +78,10 @@ window.onload = function() {
 
 		element.setAttribute("href", "mailto:" + email);
 	});
+
+	// Attach overtlay handlers only when on projects main page
+	if (document.getElementById("projects")) {
+		openOverlay();
+		closeOverlay();
+	}
 };
